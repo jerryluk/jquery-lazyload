d1c9f7422f1763d129f4ae51d8b0c57e99dd1dec
Jerry Luk <jerryluk@gmail.com>
2011-06-30 19:24:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# jQuery LazyLoad
+
+This is a jQuery plugin for lazy loading images. Images outside of viewport won't be loaded before user scrolls to them. This plugin works with Google Chrome, Safari 5, Firefox 4, IE7, IE8, and IE9
+
+This plugin is modified after Mika Tuupola's lazyload which does not work in some latest browsers.
+
+##How to use?
+
+For your HTML markup, please use data-src attribute instead of src:
+
+    <img data-src="/path_to_your_real_image">
+
+In your Javascipt:
+
+    $("img").lazyload();
+
+##Options
+
+threshold: how close to the edge image shoudl come before it is loaded. Default is 0
+placeholder: placeholder image
+container: jQuery object of the container, default is $(window)
+delay: This will load the images with a small delay, such that when user scrolls down to bottom, it will skip loading some images that are not going to be seen. Default is 100
+
+Example
+
+    $("div.container img").lazyload({
+      threshold: 200,
+      container: $("div.container"),
+      placeholder: "/gray.png",
+      delay: 50
+    });
+
+
+##Misc
+
+You can trigger the detection by triggering a "lazyload" event on the container, e.g.
+
+    $("div.container").trigger("lazyload");
+
+Please feel free to let me know if you have any questions!
+  
---
+++ jquery.lazyload.js
@@ -0,0 +1,129 @@
+/*
+ * Lazy Load - jQuery plugin for lazy loading images
+ *
+ * Copyright (c) 2011 Jerry Luk
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Modified after Mika Tuupola's lazyload:
+ *   http://www.appelsiini.net/projects/lazyload
+ *
+ * Version:  1.0.0
+ *
+ */
+ (function($) {
+
+    $.fn.lazyload = function(options) {
+        var settings = {
+            threshold: 0,
+            failurelimit: 0,
+            delay: 100,
+            container: window
+        };
+
+        if (options) {
+            $.extend(settings, options);
+        }
+        
+        function checkElements(event) {
+            var counter = 0;
+            elements.each(function() {
+                if ($(this).is(":visible") && $(this).data("src")){
+                  if ($.abovethetop(this, settings) ||
+                  $.leftofbegin(this, settings)) {
+                      /* Nothing. */
+                  } else if (!$.belowthefold(this, settings) &&
+                  !$.rightoffold(this, settings)) {
+                      $(this).trigger("appear");
+                  } else {
+                      if (counter++ > settings.failurelimit) {
+                          return false;
+                      }
+                  }
+                }
+            });
+        }
+
+        /* Fire one scroll event per scroll. Not one scroll event per image. */
+        var elements = this,
+          timeout = null;
+        $(settings.container).bind("scroll lazyload",
+          function(e) {
+            if (timeout) {
+              clearTimeout(timeout);
+            }
+            timeout = setTimeout(function() {
+              checkElements(e);
+            }, options.delay);
+          }
+        );
+
+        this.each(function() {
+            var $this = $(this);
+            if (settings.placeholder) {
+                $this.attr("src", settings.placeholder);  
+            }
+            $this.one("appear",
+              function(e) {
+                  $this.attr("src", $this.data("src"));
+                  $(this).data("src", null);
+              });
+        });
+
+        /* Force initial check if images should appear. */
+        $(settings.container).trigger("lazyload");
+
+        return this;
+
+    };
+
+    /* Convenience methods in jQuery namespace.           */
+    /* Use as  $.belowthefold(element, {threshold : 100, container : window}) */
+
+    $.belowthefold = function(element, settings) {
+        if (settings.container === undefined || settings.container === window) {
+            var fold = $(window).height() + $(window).scrollTop();
+        } else {
+            var fold = $(settings.container).offset().top + $(settings.container).height();
+        }
+        return fold <= $(element).offset().top - settings.threshold;
+    };
+
+    $.rightoffold = function(element, settings) {
+        if (settings.container === undefined || settings.container === window) {
+            var fold = $(window).width() + $(window).scrollLeft();
+        } else {
+            var fold = $(settings.container).offset().left + $(settings.container).width();
+        }
+        return fold <= $(element).offset().left - settings.threshold;
+    };
+
+    $.abovethetop = function(element, settings) {
+        if (settings.container === undefined || settings.container === window) {
+            var fold = $(window).scrollTop();
+        } else {
+            var fold = $(settings.container).offset().top;
+        }
+        return fold >= $(element).offset().top + settings.threshold + $(element).height();
+    };
+
+    $.leftofbegin = function(element, settings) {
+        if (settings.container === undefined || settings.container === window) {
+            var fold = $(window).scrollLeft();
+        } else {
+            var fold = $(settings.container).offset().left;
+        }
+        return fold >= $(element).offset().left + settings.threshold + $(element).width();
+    };
+    /* Custom selectors for your convenience.   */
+    /* Use as $("img:below-the-fold").something() */
+
+    $.extend($.expr[':'], {
+        "below-the-fold": "$.belowthefold(a, {threshold : 0, container: window})",
+        "above-the-fold": "!$.belowthefold(a, {threshold : 0, container: window})",
+        "right-of-fold": "$.rightoffold(a, {threshold : 0, container: window})",
+        "left-of-fold": "!$.rightoffold(a, {threshold : 0, container: window})"
+    });
+
+})(jQuery);
